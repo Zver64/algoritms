f8d468d2844af6df6eb0d5dee13b73bdf761f47e
Vasilii Chernov <vmchernov@inno.tech>
2023-10-02 10:28:39 +0300

    refactor

--- src/algoritms/curry.ts
+++ src/algoritms/curry.ts
@@ -5,10 +5,10 @@ function sumThree(a, b, c) {
 }
 
 function curry(cb) {
-  let allArgs = []
+  const allArgs = []
 
   return function curried(...args) {
-    allArgs = allArgs.concat(args)
+    allArgs.push(...args)
     if (allArgs.length >= cb.length) {
       return cb(...allArgs)
     }
@@ -18,10 +18,10 @@ function curry(cb) {
 
 assert.strictEqual(curry(sumThree)(2)(2)(2), sumThree(2, 2, 2))
 
-function curryCheck(func) {
+function curryCheck(cb) {
   return function curried(...args) {
-    if (args.length >= func.length) {
-      return func.apply(this, args)
+    if (args.length >= cb.length) {
+      return cb.apply(this, args)
     }
     return function (...args2) {
       return curried.apply(this, args.concat(args2))

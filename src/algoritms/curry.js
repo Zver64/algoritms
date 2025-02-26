7868fd0f9a00ba2da3b7ba18fb14668c11c2e342
Vasilii Chernov <vmchernov@inno.tech>
2025-02-26 21:57:37 +0300

    prettier

--- src/algoritms/curry.js
+++ src/algoritms/curry.js
@@ -1,45 +1,44 @@
-import assert from 'assert'
+import assert from 'assert';
 
 function sumThree(a, b, c) {
-  return a + b + c
+  return a + b + c;
 }
 
 function curry(cb) {
-  const allArgs = []
+  const allArgs = [];
 
   return function curried(...args) {
-    allArgs.push(...args)
+    allArgs.push(...args);
     if (allArgs.length >= cb.length) {
-      return cb(...allArgs)
+      return cb(...allArgs);
     }
-    return curried
-  }
+    return curried;
+  };
 }
 
 function curry2(cb) {
-
   return function curried(...args) {
     if (args.length >= cb.length) {
       return cb(...args);
     } else {
       return function (...rest) {
-        return curried(...args, ...rest)
-      }
+        return curried(...args, ...rest);
+      };
     }
-  }
+  };
 }
 
-assert.strictEqual(curry(sumThree)(2)(2)(2), sumThree(2, 2, 2))
-assert.strictEqual(curry2(sumThree)(2)(2)(2), sumThree(2, 2, 2))
+assert.strictEqual(curry(sumThree)(2)(2)(2), sumThree(2, 2, 2));
+assert.strictEqual(curry2(sumThree)(2)(2)(2), sumThree(2, 2, 2));
 
 function curryCheck(cb) {
   return function curried(...args) {
     if (args.length >= cb.length) {
-      return cb.apply(this, args)
+      return cb.apply(this, args);
     }
     return function (...args2) {
-      return curried.apply(this, args.concat(args2))
-    }
-  }
+      return curried.apply(this, args.concat(args2));
+    };
+  };
 }
-assert.strictEqual(curryCheck(sumThree)(2)(2)(2), sumThree(2, 2, 2))
+assert.strictEqual(curryCheck(sumThree)(2)(2)(2), sumThree(2, 2, 2));

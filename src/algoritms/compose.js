81ba8165808cb50a0aeabbd2f6eb2c3b0d12ba71
Vasilii Chernov <vmchernov@inno.tech>
2025-02-26 20:06:05 +0300

    compose fn

---
+++ src/algoritms/compose.js
@@ -0,0 +1,15 @@
+import assert from 'assert'
+
+export function compose(...fns) {
+  return function(...inputs) {
+    return fns.reduceRight((acc, currFn) => {
+      return [currFn(...acc)];
+    }, inputs)[0]
+  }
+}
+
+const summ = (a,b) => a + b
+const multiply = x => x * 2
+const add = x => x + 2
+
+assert.strictEqual(compose(add, multiply, summ)(2,1), 8)
